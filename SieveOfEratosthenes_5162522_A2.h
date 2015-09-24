f8b7b4f6fc973440554aeba02a08c74348552080
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-09-24 10:10:17 -0400

    Initial commit.

---
+++ SieveOfEratosthenes_5162522_A2.h
@@ -0,0 +1,14 @@
+//
+//  SieveOfEratosthenes_5162522_A2.h
+//  
+//
+//  Created by Alicia Rodriguez on 9/24/15.
+//
+//
+
+#ifndef SieveOfEratosthenes_5162522_A2_h
+#define SieveOfEratosthenes_5162522_A2_h
+
+#include <stdio.h>
+
+#endif /* SieveOfEratosthenes_5162522_A2_h */
