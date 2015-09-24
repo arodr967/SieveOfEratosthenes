e2fa463229bda0779b1355c6db29b5f65bd9ea5c
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-09-24 10:45:30 -0400

    Added disclaimer.

--- SieveOfEratosthenes_5162522_A2_main.c
+++ SieveOfEratosthenes_5162522_A2_main.c
@@ -1,10 +1,13 @@
-//
-//  SieveOfEratosthenes_5162522_A2_main.c
-//  
-//
-//  Created by Alicia Rodriguez on 9/24/15.
-//  The Main function
-//
+/*
+ File name: SieveOfEratosthenes_5162522_A2_main.c
+ 
+ Disclaimer: I hereby certify that this collective work is my own and none of it is the work of any other person or entity.
+ Created by Alicia Rodriguez on 9/24/15.
+ Panther ID: 5162522
+ 
+ Description:
+ The Main function file.
+*/
 
 #include "SieveOfEratosthenes_5162522_A2_main.h"
 #include <stdio.h>
