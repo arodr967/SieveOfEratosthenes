43403e3caffc6892990183b09b69ebf6b61e8a0d
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-09-24 10:39:51 -0400

    Changed name and added main function.

--- SieveOfEratosthenes_5162522_A2_main.c
+++ SieveOfEratosthenes_5162522_A2_main.c
@@ -1,9 +1,17 @@
 //
-//  SieveOfEratosthenes_5162522_A2.c
+//  SieveOfEratosthenes_5162522_A2_main.c
 //  
 //
 //  Created by Alicia Rodriguez on 9/24/15.
-//
+//  The Main function
 //
 
-#include "SieveOfEratosthenes_5162522_A2.h"
+#include "SieveOfEratosthenes_5162522_A2_main.h"
+#include <stdio.h>
+#include <math.h>
+
+/* Must set the return value to void */
+void main()
+{
+    
+}
